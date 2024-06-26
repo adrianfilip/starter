03bf3609146dc9636f6d7273dc8c43767e586567
Adrian Filip <realadrianfilip@gmail.com>
2024-06-26 22:10:09 +0300

    add json plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.scala" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
