000556d1b595fdcd622676a6f62e0dc2885059cb
Adrian Filip <realadrianfilip@gmail.com>
2024-07-01 15:39:36 +0300

    S-h, S-l, S-m - use default behaviour

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Set <S-h> to its default behavior (move to the top of the screen)
+vim.api.nvim_set_keymap("n", "<S-h>", "H", { noremap = true, silent = true })
+
+-- Set <S-m> to its default behavior (move to the middle of the screen)
+vim.api.nvim_set_keymap("n", "<S-m>", "M", { noremap = true, silent = true })
+
+-- Set <S-l> to its default behavior (move to the bottom of the screen)
+vim.api.nvim_set_keymap("n", "<S-l>", "L", { noremap = true, silent = true })
