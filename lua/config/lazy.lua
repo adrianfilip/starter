b1d97edca4ec5944af67dc14a7fa5809e6297279
Adrian Filip <realadrianfilip@gmail.com>
2024-10-29 22:33:43 +0200

    add nvim-web-devicons

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,3 +58,9 @@ require("lazy").setup({
     },
   },
 })
+
+require("nvim-web-devicons").setup({
+  -- globally enable default icons (default to false)
+  -- will get overriden by `get_icons` option
+  default = true,
+})
