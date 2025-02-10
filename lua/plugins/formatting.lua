9db9b01a0cfe5fa0fa190d67798472cf5f173829
Adrian Filip <realadrianfilip@gmail.com>
2025-02-10 23:18:47 +0200

    remove prepend_args from prettier formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -15,7 +15,7 @@ return {
       },
       formatters = {
         prettier = {
-          prepend_args = { "--tab-width", "4" },
+          --          prepend_args = { "--tab-width", "4" },
         },
       },
     },
