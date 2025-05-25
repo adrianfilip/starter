413c08283f19f89f48bbe75b9611245e5ea38c83
Adrian Filip <realadrianfilip@gmail.com>
2025-05-25 11:54:00 +0300

    fix: enter to select suggestion was exiting nvim

--- lua/plugins/copilot-cs.lua
+++ lua/plugins/copilot-cs.lua
@@ -4,12 +4,32 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      suggestion = { enabled = false },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
       panel = { enabled = false },
       filetypes = {
         markdown = true,
         help = true,
+        ["*"] = true,
       },
     },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+        ["<C-l>"] = cmp.mapping.confirm({ select = true }), -- ✅ use <C-l> for completion
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = function() end, -- ❌ disable Enter entirely
+      })
+    end,
   },
 }
