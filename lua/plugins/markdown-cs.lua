8a63727770d6d20e2e2252c8162b45a3b539cf02
Adrian Filip <realadrianfilip@gmail.com>
2026-02-27 10:31:39 +0200

    always show quotes in markdown files

--- lua/plugins/markdown-cs.lua
+++ lua/plugins/markdown-cs.lua
@@ -1,3 +1,11 @@
 return {
   { "jannis-baum/vivify.vim" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      win_options = {
+        conceallevel = { rendered = 0 },
+      },
+    },
+  },
 }
